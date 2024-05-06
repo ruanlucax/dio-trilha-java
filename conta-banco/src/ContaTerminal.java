899913c9cd7861879fbc851813840765ed905d94
ruanlucax <ruan07lima@gmail.com>
2024-05-06 19:34:22 -0300

    Conta banco atualizada.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.print("Por favor, digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Agora digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+        
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque." );
+   
+   
     }
+
 }
+
